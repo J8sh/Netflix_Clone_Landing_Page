63566287d483bbda1d0ed9eba0cca0ac75e9c481
Joshua Garcia <Superexpert12@Joshuas-MacBook-Pro.local>
2019-07-06 09:55:24 -0500

    Uploading code 7/6/19

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    //add show class
+    tabContentItem.classList.add('show');
+}
+
+// this will erase the border, so only one border shows at a time
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
